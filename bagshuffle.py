7e958bd922c4ed919645eb793a32b42ae447f383
Jacob <jacob.bengtsson@rocketmail.com>
2022-06-17 19:17:25 +0200

    Add description comment

--- bagshuffle.py
+++ bagshuffle.py
@@ -1,14 +1,13 @@
 from random import shuffle
 
-
-import random
+# Repeatedly shuffles a list of items using the bag method (well known from modern Tetris), and prints the shuffled list.
 
 bag = ["L-Piece", "I-Piece", "T-Piece", "S-Piece", "Z-Piece", "J-Piece", "O-Piece"]
 
 stringToBePrinted = ""
 
 for i in range(20):
-  random.shuffle(bag)
+  shuffle(bag)
   for thing in bag:
     stringToBePrinted += thing + "\r\n"
 
